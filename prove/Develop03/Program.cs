ea9e495bcc70ea82a46d3eeb2f1c219ecec3afe2
Jose Diaz <dia20013@byui.edu>
2024-01-26 21:50:18 -0500

    Corrected comment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     /*In order to exceed the requirements I did the following:
     (1) Added a Display Class to better manage the input and output improving the encapsulation.
-    (2) Added Getters and Setters.*/
+    (2) Added Getters and Setters for the Word Class.*/
     static void Main(string[] args)
     {
         //Console.WriteLine("Hello Develop03 World!");
